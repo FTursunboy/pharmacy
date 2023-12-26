e1d9d788645af641a2d8bf113e8033ede211ebc8
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-12-26 18:13:36 +0500

    fds

--- database/migrations/2023_12_22_062314_make_column_status_nullabel_to_users_table.php
+++ database/migrations/2023_12_22_062314_make_column_status_nullabel_to_users_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('status')->nullable()->change();
-        });
+//        Schema::table('users', function (Blueprint $table) {
+//            $table->string('status')->nullable()->change();
+//        });
     }
 
     /**
