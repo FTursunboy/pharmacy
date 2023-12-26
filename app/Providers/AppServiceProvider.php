c029e8f8fc5a4ca85ee0af611f7540e8bcd91abf
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-12-26 18:38:37 +0500

    banners, activecategories api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Services\AuthService;
+use App\Services\BannerService;
 use App\Services\Contracts\AuthServiceInterface;
+use App\Services\Contracts\BannerServiceInterface;
 use App\Services\Contracts\ProductCategoryServiceInterface;
 use App\Services\Contracts\ProductServiceInterface;
 use App\Services\Contracts\ShopServiceInterface;
@@ -26,6 +28,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(ProductServiceInterface::class, ProductService::class);
         $this->app->singleton(UserServiceInterface::class, UserService::class);
         $this->app->singleton(ShopServiceInterface::class, ShopService::class);
+        $this->app->singleton(BannerServiceInterface::class, BannerService::class);
     }
 
     /**
